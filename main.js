d2c975821cac8ee4a60fbfb77efe8262e31ac224
Shmarte1970 <shmarte@gmail.com>
2023-06-13 08:57:57 +0200

    Mejorado

--- main.js
+++ main.js
@@ -17,11 +17,11 @@ document.getElementById('converter-form').addEventListener('submit', function(e)
         result.innerHTML += '<p>' + valor + ' px equivale a ' + (valor / 16 * 0.5) + ' ex.</p>';
     } else if (unit === 'em' && convert === 'px') {
         result.innerHTML += '<p>' + valor + ' em equivale a ' + (valor * 16) + ' px.</p>';
-    } else if (unit === 'ex' && convert === 'px') {
+    } else if (unit === 'px' && convert === 'ex') {
         result.innerHTML += '<p>' + valor + ' ex equivale a ' + (valor * 8) + ' px.</p>';
-    } else if (unit === '%' && convert === 'px') {
+    } else if (unit === 'px' && convert === '%') {
         result.innerHTML += '<p>' + valor + '% equivale a ' + (valor / 100) + ' veces el tamaño del contenedor.</p>';
-    } else if (unit === 'rem' && convert === 'px') {
+    } else if (unit === 'px' && convert === 'rem') {
         result.innerHTML += '<p>' + valor + ' rem equivale a ' + valor + ' veces el tamaño de la raíz.</p>';
     } else {
         result.innerHTML += '<p>' + ' Operacion no disponible ' + '<p/>';
